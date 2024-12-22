c44ab0a6df650c072b63bdb57ac0196e2fc91863
Yngwie Kurebayashi <yngwie.rs2@gmail.com>
2024-12-22 16:25:32 +0000

    feat: adds fallback div in case of not having info selected

--- src/components/Info/components/DetailedInfo/index.tsx
+++ src/components/Info/components/DetailedInfo/index.tsx
@@ -7,29 +7,54 @@ export const DetailedInfo = () => {
   const myContext = useContext(MyContext);
 
   return (
-    <Column>
-      <TitleContainer>
-        <AssetTitle>{myContext?.selectedAsset?.name}</AssetTitle>
-      </TitleContainer>
+    <>
+      {myContext?.selectedAsset ? (
+        <Column $disableBorder>
+          <TitleContainer>
+            <AssetTitle>{myContext?.selectedAsset?.name}</AssetTitle>
+          </TitleContainer>
 
-      <Column $padding="24px">
-        <Column $marginBottom="24px"></Column>
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            justifyContent: "space-between",
-          }}
-        >
-          <Column $disableBorder>
-            <p>Sensor</p> <p>{myContext?.selectedAsset?.sensorId}</p>
+          <Column $padding="24px" $disableBorder>
+            <Row>
+              {/* Placeholder para imagem, pode ser um input de arquivo */}
+              <div
+                style={{
+                  minWidth: "336px",
+                  height: "226px",
+                  border: "1px dashed #55A6FF",
+                  backgroundColor: "#F2F8FF",
+                  marginBottom: "24px",
+                }}
+              />
+              <Column $disableBorder>
+                <Column style={{ paddingBottom: "24px" }}>
+                  <Infotitle>Tipo de equipamento:</Infotitle>
+                  <InfoText>Tipo A</InfoText>
+                </Column>
+                <Column $disableBorder style={{ paddingTop: "24px" }}>
+                  <Infotitle>Responsáveis:</Infotitle>
+                  <InfoText>Responsável A</InfoText>
+                </Column>
+              </Column>
+            </Row>
+            <Row $disableBorder>
+              <Column $disableBorder>
+                <Infotitle>Sensor</Infotitle>
+                <InfoText>{myContext?.selectedAsset?.sensorId}</InfoText>
+              </Column>
+              <Column $disableBorder>
+                <Infotitle>Receptor</Infotitle>
+                <InfoText>{myContext?.selectedAsset?.gatewayId}</InfoText>
+              </Column>
+            </Row>
           </Column>
-          <Column $disableBorder>
-            <p>Receptor</p> <p>{myContext?.selectedAsset?.gatewayId}</p>
-          </Column>
-        </div>
-      </Column>
-    </Column>
+        </Column>
+      ) : (
+        <FallbackDiv>
+          <p>Selecione um asset para acessar as suas informações</p>
+        </FallbackDiv>
+      )}
+    </>
   );
 };
 
@@ -51,6 +76,19 @@ export const Column = styled.div<{
   `}
 `;
 
+export const Row = styled.div<{
+  $disableBorder?: boolean;
+}>`
+  display: flex;
+  width: 100%;
+  margin-bottom: 24px;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+  border-bottom: ${(props) =>
+    !props.$disableBorder && `1px solid ${CustomColors.grey.light}`};
+`;
+
 export const AssetTitle = styled.p`
   font-size: 18px;
   font-weight: 600;
@@ -64,4 +102,24 @@ export const TitleContainer = styled.div`
   justify-content: flex-start;
   align-items: center;
   border-bottom: 1px solid ${CustomColors.grey.medium};
+`;
+
+export const Infotitle = styled.p`
+  padding: 8px 0;
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+export const InfoText = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${CustomColors.grey.dark};
+`;
+
+export const FallbackDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
