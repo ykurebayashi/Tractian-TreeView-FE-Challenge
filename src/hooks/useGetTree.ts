54d45ea8c4f822f8c7e45c1b24a2a38054774437
Yngwie Kurebayashi <yngwie.rs2@gmail.com>
2024-12-22 20:17:30 +0000

    refactor: adjust code comments and function names

--- src/hooks/useGetTree.ts
+++ src/hooks/useGetTree.ts
@@ -37,100 +37,89 @@ export const useGetTree = ({currentId, search, filter}: {currentId: string, sear
   }, [locations, assets])
 
   const buildTree = (locations: Location[], assets: Asset[], filter?: FilterType): TreeNode[] => {
-      const locationMap = new Map<string, TreeNode>(); // usar map pra depois poder acessar os nodes baseado na chave do seu id em vez de ter que ficar procurando no array.
-      const assetMap = new Map<string, TreeNode>();
-      const rootNodes: TreeNode[] = []; // aqui como vai ser a versão final, não tem problema jogar como array
-    
-
-      //TO-DO -> tentar colocar os dois em 1 loop só, dessa forma eu estou passando 2x por cada locations e cada assets
-      locations?.forEach((location) => {
-        locationMap.set(location.id, {
-          ...location,
-          id: location.id,
-          name: location.name,
-          type: "location", // poderia ser sublocation, mas não tem diferença a nível funcional pra gente, já que o parentId que vai determinar
+    const locationMap = new Map<string, TreeNode>(); 
+    const assetMap = new Map<string, TreeNode>();
+    const rootNodes: TreeNode[] = []; 
+  
+    locations?.forEach((location) => {
+      locationMap.set(location.id, {
+        ...location,
+        id: location.id,
+        name: location.name,
+        type: "location", 
+        children: [],
+      });
+    });
+  
+    assets?.forEach((asset) => {
+      if (!filter) {
+        assetMap.set(asset.id, {
+          ...asset,
+          id: asset.id,
+          name: asset.name,
+          type: asset.sensorType ? "component" : "asset",
           children: [],
         });
-      });
-    
-      assets?.forEach((asset) => {
-        if (!filter) {
-          assetMap.set(asset.id, {
-            ...asset,
-            id: asset.id,
-            name: asset.name,
-            type: asset.sensorType ? "component" : "asset", // Se tiver sensorType, é 'component', caso contrário é 'asset'
-            children: [],
-          });
-        }
+      }
 
-        if (filter === 'energy_sensor' && (asset.sensorType === 'energy' || asset.sensorType === null)) {
-          assetMap.set(asset.id, {
-            ...asset,
-            id: asset.id,
-            name: asset.name,
-            type: asset.sensorType ? "component" : "asset",
-            children: [],
-          });
-        }
-      
-        if (filter === 'critical_sensor' && asset.status !== 'operating') {
-          assetMap.set(asset.id, {
-            ...asset,
-            id: asset.id,
-            name: asset.name,
-            type: asset.sensorType ? "component" : "asset",
-            children: [],
-          });
-        }
-      });
-      
-
-      // 4 regras:
-      // 1)If the item has a sensorType, it means it is a component. If it does not have a location or a parentId, it means he is unliked from any asset or location in the tree.
-      // 2)If the item has a sensorType, it means it is a component. If it does have a location or a parentId, it means he has an asset or Location as parent
-      // 3) If the item has a parentId and does not have a sensorId, it means he is an asset with another asset as a parent
-      // 4) If the item has a location and does not have a sensorId, it means he is an asset with a location as parent, from the location collection
-      locations.forEach((location) => {
-          if (location.parentId) { // If the Location has a parentId, it means it is a sub location - isos significa que se tem parentId entao eu devo colocar ele sob o children do pai.
-            const parent = locationMap.get(location.parentId); // pega o elemento pai: usando o proprio parentId
-            const current = locationMap.get(location.id); // pega o proprio elemento
-            if (parent && current) { // checagem que esses elementos foram encontrados
-              parent.children.push(current); // coloca o atual elemento sob a aba de filho do do elemento pai
-            }
-          } else { // caso não tenha parentId, ele é um local proprio
-            const current = locationMap.get(location.id);  // entao eu pego ele
-            if (current) {
-              rootNodes.push(current); // e jogo direto no node (é um local e nao um sub local)
-            }
-          }
-      });
-      
-      assets.forEach((asset) => {
-          if (asset.locationId) {
-            const parent = locationMap.get(asset.locationId);
-            const current = assetMap.get(asset.id);
-            if (parent && current) {
-              parent.children.push(current);
-            }
-          } else if (asset.parentId) { 
-            const parent = assetMap.get(asset.parentId);
-            const current = assetMap.get(asset.id);
-            if (parent && current) {
-              parent.children.push(current);
-            }
-          } else { //  If it does not have a location or a parentId, it means he is unliked from any asset or location in the tree - como ele já passou pelo parentId e pelo locationId, eu sei que ele é um asset solto, jogo direto nos roots.
-            const current = assetMap.get(asset.id);
-            if (current) {
-              rootNodes.push(current);
-            }
-          }
-      });
+      if (filter === 'energy_sensor' && (asset.sensorType === 'energy' || asset.sensorType === null)) {
+        assetMap.set(asset.id, {
+          ...asset,
+          id: asset.id,
+          name: asset.name,
+          type: asset.sensorType ? "component" : "asset",
+          children: [],
+        });
+      }
     
-      return rootNodes;
+      if (filter === 'critical_sensor' && asset.status !== 'operating') {
+        assetMap.set(asset.id, {
+          ...asset,
+          id: asset.id,
+          name: asset.name,
+          type: asset.sensorType ? "component" : "asset",
+          children: [],
+        });
+      }
+    });
+    
+    locations.forEach((location) => {
+      const current = locationMap.get(location.id);
+        if (location.parentId) { 
+          const parent = locationMap.get(location.parentId); 
+          if (parent && current) {
+            parent.children.push(current);
+          }
+        } else {
+          if (current) {
+            rootNodes.push(current); 
+          }
+        }
+    });
+    
+    assets.forEach((asset) => {
+      const current = assetMap.get(asset.id)
+        if (asset.locationId) {
+          const parent = locationMap.get(asset.locationId);
+          if (parent && current) {
+            parent.children.push(current);
+          }
+        } else if (asset.parentId) { 
+          const parent = assetMap.get(asset.parentId);
+          if (parent && current) {
+            parent.children.push(current);
+          }
+        } else {
+          if (current) {
+            rootNodes.push(current);
+          }
+        }
+    });
+  
+    return rootNodes;
   }
 
-  const filterHighlightedNodes = useCallback(
+  const filterRelevantNodes = useCallback(
     (nodes: NodeType[], query: string): NodeType[] => {
       const filteredNodes: NodeType[] = [];
 
@@ -140,7 +129,7 @@ export const useGetTree = ({currentId, search, filter}: {currentId: string, sear
           : false;
 
         const nodeChildrens = node.children
-          ? filterHighlightedNodes(node.children, query)
+          ? filterRelevantNodes(node.children, query)
           : [];
 
         if (shouldInsert || nodeChildrens.length > 0) {
@@ -168,8 +157,8 @@ export const useGetTree = ({currentId, search, filter}: {currentId: string, sear
     if (!search) {
       return data;
     }
-    return filterHighlightedNodes(data, search);
-  }, [filterHighlightedNodes, search, data]);
+    return filterRelevantNodes(data, search);
+  }, [filterRelevantNodes, search, data]);
 
 
   return {
